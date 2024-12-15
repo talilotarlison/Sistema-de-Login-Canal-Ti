0f0b34f688efeb4a51e60d60aa0d7cc192689931
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2024-12-15 16:47:59 -0300

    index

--- index.php
+++ index.php
@@ -1,57 +1,57 @@
-&lt;?php
+<?php
 session_start();
-?&gt;
+?>
 
-&lt;!DOCTYPE html&gt;
-&lt;html&gt;
+<!DOCTYPE html>
+<html>
     
-&lt;head&gt;
-    &lt;meta charset="utf-8"&gt;
-    &lt;meta http-equiv="X-UA-Compatible" content="IE=edge"&gt;
-    &lt;meta name="viewport" content="width=device-width, initial-scale=1"&gt;
-    &lt;title&gt;Sistema de Login - PHP + MySQL - Canal TI&lt;/title&gt;
-    &lt;link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet"&gt;
-    &lt;link rel="stylesheet" href="css/bulma.min.css" /&gt;
-    &lt;link rel="stylesheet" type="text/css" href="css/login.css"&gt;
-&lt;/head&gt;
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
 
-&lt;body&gt;
-    &lt;section class="hero is-success is-fullheight"&gt;
-        &lt;div class="hero-body"&gt;
-            &lt;div class="container has-text-centered"&gt;
-                &lt;div class="column is-4 is-offset-4"&gt;
-                    &lt;h3 class="title has-text-grey"&gt;Sistema de Login&lt;/h3&gt;
-                    &lt;h3 class="title has-text-grey"&gt;&lt;a href="https://youtube.com/canaltioficial" target="_blank"&gt;Canal TI&lt;/a&gt;&lt;/h3&gt;
-                    &lt;?php
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Sistema de Login</h3>
+                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">Canal TI</a></h3>
+                    <?php
                     if(isset($_SESSION['nao_autenticado'])):
-                    ?&gt;
-                    &lt;div class="notification is-danger"&gt;
-                      &lt;p&gt;ERRO: Usuário ou senha inválidos.&lt;/p&gt;
-                    &lt;/div&gt;
-                    &lt;?php
+                    ?>
+                    <div class="notification is-danger">
+                      <p>ERRO: Usuário ou senha inválidos.</p>
+                    </div>
+                    <?php
                     endif;
                     unset($_SESSION['nao_autenticado']);
-                    ?&gt;
-                    &lt;div class="box"&gt;
-                        &lt;form action="login.php" method="POST"&gt;
-                            &lt;div class="field"&gt;
-                                &lt;div class="control"&gt;
-                                    &lt;input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus=""&gt;
-                                &lt;/div&gt;
-                            &lt;/div&gt;
+                    ?>
+                    <div class="box">
+                        <form action="login.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
+                                </div>
+                            </div>
 
-                            &lt;div class="field"&gt;
-                                &lt;div class="control"&gt;
-                                    &lt;input name="senha" class="input is-large" type="password" placeholder="Sua senha"&gt;
-                                &lt;/div&gt;
-                            &lt;/div&gt;
-                            &lt;button type="submit" class="button is-block is-link is-large is-fullwidth"&gt;Entrar&lt;/button&gt;
-                        &lt;/form&gt;
-                    &lt;/div&gt;
-                &lt;/div&gt;
-            &lt;/div&gt;
-        &lt;/div&gt;
-    &lt;/section&gt;
-&lt;/body&gt;
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
+                                </div>
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
 
-&lt;/html&gt;
+</html>
